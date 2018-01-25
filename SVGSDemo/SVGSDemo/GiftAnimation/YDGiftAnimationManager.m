0c06e8128d6cb00c47da8aef57ff57d0f038d81a
xuning <xuninghsu@gmail.com>
2018-01-25 09:55:15 +0800

    简化代码

--- SVGSDemo/SVGSDemo/GiftAnimation/YDGiftAnimationManager.m
+++ SVGSDemo/SVGSDemo/GiftAnimation/YDGiftAnimationManager.m
@@ -70,7 +70,8 @@ static YDGiftAnimationManager *instance;
 }
 
 + (void)attempDealloc {
-    
+    instance.grounderArray = nil;
+    instance.modelArray = nil;
     onceToken = 0; // 只有置成0,GCD才会认为它从未执行过.它默认为0.这样才能保证下次再次调用shareInstance的时候,再次创建对象.
     instance = nil;
 
